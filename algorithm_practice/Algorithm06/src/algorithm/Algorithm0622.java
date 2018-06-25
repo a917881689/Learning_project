f805dd1ee1ee049e38f8f2530580c0007decfe04
a917881689 <a917881689@gmail.com>
2018-06-25 17:38:56 +0800

    Revert " Algorithm"
    This reverts commit bdff1e8b5577867fc3f71257627bd398bda3ac50.

--- algorithm_practice/Algorithm06/src/algorithm/Algorithm0622.java
+++ algorithm_practice/Algorithm06/src/algorithm/Algorithm0622.java
@@ -1,6 +1,11 @@
 package algorithm;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.regex.Pattern;
 
 /**
@@ -10,13 +15,25 @@ import java.util.regex.Pattern;
 public class Algorithm0622 {
     public static void main(String[] args) {
 
-        //数组末尾数值+1
-        //algorithm();
-        //验证给定的字符串是否为数字。
-        algorithm1("0.1");
-        algorithm1("0");
-        algorithm1("222e10");
-        algorithm1("1 a");
+        // 数组末尾数值+1
+        // algorithm();
+        // 验证给定的字符串是否为数字。
+//        algorithm1("1e.66");
+//        algorithm1("0");
+//        algorithm1("222e10");
+//        algorithm1(".e9");
+//        algorithm1("1e.");
+//        algorithm1("-1.");
+//        algorithm1("07L");
+//        algorithm1("+e3");
+//        algorithm1("6e6.5");
+        DateFormat dateF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(dateF.format(new Date()));
+        System.out.println(dateF.format(System.currentTimeMillis()));
+        Calendar calendar = Calendar.getInstance();
+        System.out.printf("%d/%d/%d  %d/%d/%d",calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DATE),
+                calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),calendar.get(Calendar.SECOND));
+
     }
     public static void algorithm () {
         Solution1 s = new Solution1();
@@ -33,10 +50,56 @@ class Solution1 {
     public boolean isNumber(String s) {
         String str = s.trim();
         ArrayList<Character> list = new ArrayList<>();
-        int count1 = 0;
         for (int i = 0;i < str.length();i++) {
-            list.add(s.charAt(i));
+            if (i == 0 && (str.charAt(i) == '-' || str.charAt(i) == '+')) {
+                continue;
+            }
+            list.add(str.charAt(i));
         }
+        ArrayList<Character> list2 = new ArrayList<>();
+
+        int count1 = -1;
+        int count2 = -1;
+        int count3 = -1;
+        int count4 = -1;
+
+        for (int i = 0;i < list.size();i++) {
+            char c = list.get(i);
+            if (c >= '0' && c <= '9') {
+                list2.add(c);
+            }else if (c == '.') {
+                if (count1 == -1) {
+                    count1 = list2.size();
+                    count3 = i;
+                }else {
+                    return false;
+                }
+            }else if (c == 'e' || c == 'E') {
+                if (count2 == -1 && i != 0 && i+1 != list.size()) {
+                    count2 = list2.size();
+                    count4 = i;
+                }else {
+                    return false;
+                }
+            }else {
+                return false;
+            }
+        }
+//        else if (str.charAt(i) == 'l' || str.charAt(i) == 'L' || str.charAt(i) == 'f' || str.charAt(i) == 'F') {
+//            if (i+1 == str.length()) {
+//                count3 = i;
+//            }else {
+//                return false;
+//            }
+//        }
+        if (count2 != -1 && (count1 == 0 || count1 == list2.size())) {
+            return false;
+        }else if (count2 != -1 && count1 != -1 && count3 > count4) {
+            return false;
+        }else if (list2.size() >= 1){
+            return true;
+        }
+        return false;
     }
 
     public int[] plusOne(int[] digits) {
