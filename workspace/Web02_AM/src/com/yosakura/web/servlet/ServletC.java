401cbf264fe812874711e19b198453560b72699e
a917881689 <a917881689@gmail.com>
2018-07-09 13:11:57 +0800

    update

---
+++ workspace/Web02_AM/src/com/yosakura/web/servlet/ServletC.java
@@ -0,0 +1,25 @@
+package com.yosakura.web.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletC extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	}
+}
